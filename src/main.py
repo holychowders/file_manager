223c79791ad63177de42cd3b5d9ccff96bfeee3b
holychowders <theholychowders@gmail.com>
2022-12-25 01:02:09 -0600

    Add basic GUI.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,25 @@
 import sqlite3
+from tkinter import Tk
 
 
 def main() -> None:
+    gui = init_gui()
+
     connection = sqlite3.connect("test.db")
     cursor = connection.cursor()
-
     cursor.execute("")
-
     print(connection)
+
+    gui.mainloop()
+
+
+def init_gui() -> Tk:
+    root = Tk()
+    root.title("File Manager")
+    root.iconbitmap("assets/main-icon-512px-colored.ico")
+    root.geometry("400x600")
+
+    return root
 
 
 if __name__ == "__main__":
