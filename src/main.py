6bc0b1678b1ac3b3aea07ccc7f15a1f521079807
holychowders <theholychowders@gmail.com>
2022-12-25 01:11:36 -0600

    Clean up.

--- src/main.py
+++ src/main.py
@@ -1,25 +1,29 @@
 import sqlite3
+from sqlite3 import Cursor
 from tkinter import Tk
 
 
 def main() -> None:
-    gui = init_gui()
+    db_cursor = get_db_cursor()
+    db_cursor.execute("")
 
+    build_gui().mainloop()
+
+
+def get_db_cursor() -> Cursor:
     connection = sqlite3.connect("test.db")
     cursor = connection.cursor()
-    cursor.execute("")
-    print(connection)
 
-    gui.mainloop()
+    return cursor
 
 
-def init_gui() -> Tk:
-    root = Tk()
-    root.title("File Manager")
-    root.iconbitmap("assets/main-icon-512px-colored.ico")
-    root.geometry("400x600")
+def build_gui() -> Tk:
+    gui = Tk()
+    gui.title("File Manager")
+    gui.iconbitmap("assets/main-icon-512px-colored.ico")
+    gui.geometry("400x600")
 
-    return root
+    return gui
 
 
 if __name__ == "__main__":
