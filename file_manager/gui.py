8fb414b756e981ab6df494c5cb8d6866a92fe5a1
holychowders <theholychowders@gmail.com>
2022-12-26 13:58:54 -0600

    Update GUI. See description.
    - Adjust window geometry.
    - Add dummy search bar for tags and files.
    - Move Tags label left.
    - Make tags list vertical.
    - Generate buttons for files (Functional. Clicking button plays video).

--- file_manager/gui.py
+++ file_manager/gui.py
@@ -1,5 +1,7 @@
+import os
 from dataclasses import dataclass
-from tkinter import LEFT, NW, Button, Checkbutton, IntVar, Label, Tk
+from functools import partial
+from tkinter import NW, TOP, Button, Checkbutton, Entry, IntVar, Label, N, Tk
 from typing import List
 
 from db import fetch_files_from_db, fetch_tags_from_db
@@ -10,36 +12,61 @@ class GUI:
         gui = Tk()
         gui.title("File Manager")
         gui.iconbitmap("assets/main-icon-512px-colored.ico")
-        gui.geometry("400x600")
+        gui.geometry("600x600")
 
         self.gui = gui
 
         if debug:
             self.debug()
 
-        self.init_content_tags()
+        self.add_search_bar()
+        self.init_content_tags_section()
+        self.init_file_results_section()
 
     def run(self) -> None:
         self.gui.mainloop()
 
     # Tags stuff
 
-    def init_content_tags(self) -> None:
+    def init_content_tags_section(self) -> None:
         self.tags: List[ContentTag] = []
-        self.fetch_tags_from_db()
+        self.load_tags_from_db()
         self.add_tags_selection()
 
     def add_tags_selection(self):
-        Label(self.gui, text="Tags").pack()
+        Label(self.gui, text="Tags").pack(anchor=NW)
+        Entry(self.gui, width=20).pack(anchor=NW, ipadx=1, ipady=1)
 
         for tag in self.tags:
-            Checkbutton(self.gui, text=tag.name, variable=tag.is_selected).pack(side=LEFT, anchor=NW)
+            Checkbutton(self.gui, text=tag.name, variable=tag.is_selected).pack(anchor=NW)
 
-    def fetch_tags_from_db(self) -> None:
+    def load_tags_from_db(self) -> None:
         tags = fetch_tags_from_db()
 
         for tag in tags:
             self.tags.append(ContentTag(tag, IntVar()))
+
+    # Search bar
+
+    def add_search_bar(self) -> None:
+        Entry(self.gui, width=35).pack(side=TOP, anchor=N, ipadx=1, ipady=1)
+
+    # File results stuff
+
+    def init_file_results_section(self) -> None:
+        self.file_results: List[FileResult] = []
+        self.load_files_from_db()
+        self.add_file_results()
+
+    def load_files_from_db(self) -> None:
+        files = fetch_files_from_db()
+
+        for name, path in files:
+            self.file_results.append(FileResult(name, path))
+
+    def add_file_results(self) -> None:
+        for file in self.file_results:
+            Button(text=file.name, command=partial(os.startfile, file.path)).pack()
 
     # Debugging stuff
 
@@ -60,3 +87,9 @@ class GUI:
 class ContentTag:
     name: str
     is_selected: IntVar
+
+
+@dataclass
+class FileResult:
+    name: str
+    path: str
