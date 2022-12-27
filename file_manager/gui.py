c56a93c33721e6ede1b90cb5ec62dd9fec3cc084
holychowders <theholychowders@gmail.com>
2022-12-27 14:40:05 -0600

    Simplify some things.

--- file_manager/gui.py
+++ file_manager/gui.py
@@ -7,6 +7,19 @@ from typing import List
 
 from db import fetch_files_from_db, fetch_tags_from_db
 
+
+@dataclass
+class ContentTag:
+    name: str
+    is_selected: IntVar
+
+
+@dataclass
+class FileResult:
+    name: str
+    path: str
+
+
 WidgetGridPosition = namedtuple("WidgetGridPosition", "row column")
 
 
@@ -25,18 +38,13 @@ class GUI:
         if debug:
             self.debug()
 
-        self.init_tags_frame()
-        self.init_files_frame()
+        self.add_tags_frame()
+        self.add_files_frame()
 
     def run(self) -> None:
         self.gui.mainloop()
 
     # Tags stuff
-
-    def init_tags_frame(self) -> None:
-        self.tags: List[ContentTag] = []
-        self.load_tags_from_db()
-        self.add_tags_frame()
 
     def add_tags_frame(self) -> None:
         row, column = self.TAGS_FRAME_POS
@@ -45,7 +53,8 @@ class GUI:
 
         self.add_tags_search_and_edit_subframe(frame)
 
-        for tag in self.tags:
+        tags = self.fetch_tags_from_db()
+        for tag in tags:
             Checkbutton(frame, text=tag.name, variable=tag.is_selected).grid()
 
     def add_tags_search_and_edit_subframe(self, tags_frame: LabelFrame) -> None:
@@ -55,24 +64,17 @@ class GUI:
         Entry(frame, width=10).grid(row=0, column=0, padx=5, pady=10, ipadx=1, ipady=1)
         Button(frame, text="+/-").grid(row=0, column=1, padx=5, pady=5)
 
-    def load_tags_from_db(self) -> None:
+    @staticmethod
+    def fetch_tags_from_db() -> List[ContentTag]:
         tags = fetch_tags_from_db()
+        content_tags = []
 
         for tag in tags:
-            self.tags.append(ContentTag(tag, IntVar()))
+            content_tags.append(ContentTag(tag, IntVar()))
+
+        return content_tags
 
     # File results stuff
-
-    def init_files_frame(self) -> None:
-        self.file_results: List[FileResult] = []
-        self.load_files_from_db()
-        self.add_files_frame()
-
-    def load_files_from_db(self) -> None:
-        files = fetch_files_from_db()
-
-        for name, path in files:
-            self.file_results.append(FileResult(name, path))
 
     def add_files_frame(self) -> None:
         row, column = self.FILES_FRAME_POS
@@ -81,8 +83,19 @@ class GUI:
 
         Entry(frame, width=35).pack(side=TOP, anchor=N, padx=5, pady=5, ipadx=1, ipady=1)
 
-        for file in self.file_results:
+        files = self.fetch_files_from_db()
+        for file in files:
             Button(frame, text=file.name, command=partial(os.startfile, file.path)).pack(padx=5, pady=5)
+
+    @staticmethod
+    def fetch_files_from_db() -> List[FileResult]:
+        files = fetch_files_from_db()
+        file_results = []
+
+        for name, path in files:
+            file_results.append(FileResult(name, path))
+
+        return file_results
 
     # Debugging stuff
 
@@ -90,22 +103,10 @@ class GUI:
         self.add_debug_button()
 
     def add_debug_button(self) -> None:
-        Button(self.gui, text="Debug", command=self.debug_db).pack()
-
-    @staticmethod
-    def debug_db() -> None:
-        print("\nDB DEBUG:")
-        print("files:\n", fetch_files_from_db())
-        print("\ntags:\n", fetch_tags_from_db())
+        Button(self.gui, text="Debug", command=debug_db).pack()
 
 
-@dataclass
-class ContentTag:
-    name: str
-    is_selected: IntVar
-
-
-@dataclass
-class FileResult:
-    name: str
-    path: str
+def debug_db() -> None:
+    print("\nDB DEBUG:")
+    print("files:\n", fetch_files_from_db())
+    print("\ntags:\n", fetch_tags_from_db())
