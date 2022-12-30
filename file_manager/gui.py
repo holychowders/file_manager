a0d455cfa6a1632797c2f13c435111dc654f70df
holychowders <theholychowders@gmail.com>
2022-12-29 18:22:42 -0600

    Use tags to filter out files from results.

--- file_manager/gui.py
+++ file_manager/gui.py
@@ -4,6 +4,7 @@ from enum import Enum
 from functools import partial
 from logging import warning
 from tkinter import TOP, Button, Checkbutton, Entry, LabelFrame, Menu, N, Tk
+from typing import List
 
 import db
 
@@ -24,7 +25,7 @@ class GUI:
         self._add_menu_bar()
         self._handle_debug_init(debug)
         self._init_tags_frame()
-        self._add_files_frame()
+        self._init_files_frame()
 
     def run(self) -> None:
         self.gui.mainloop()
@@ -88,12 +89,14 @@ class GUI:
         frame = LabelFrame(self.gui, text="Tags", bg=self.bg_color, fg=self.fg_color)
         frame.grid(row=row, column=column, padx=5, pady=5)
 
-        self._add_tags_search_and_edit_subframe(frame)
-        self._add_tags_selection_clear_button(frame)
-        self._populate_tags_in_tags_frame(frame)
+        self.tags_frame = frame
 
-    def _add_tags_search_and_edit_subframe(self, tags_frame: LabelFrame) -> None:
-        frame = LabelFrame(tags_frame, text="Search/Edit", bg=self.bg_color, fg=self.fg_color)
+        self._add_tags_search_and_edit_subframe()
+        self._add_tags_selection_clear_button()
+        self._populate_tags_in_tags_frame()
+
+    def _add_tags_search_and_edit_subframe(self) -> None:
+        frame = LabelFrame(self.tags_frame, text="Search/Edit", bg=self.bg_color, fg=self.fg_color)
         frame.grid(padx=5, pady=5)
 
         entry = Entry(frame, width=10, insertbackground=self.fg_color, bg=self.bg_color, fg=self.fg_color)
@@ -102,16 +105,16 @@ class GUI:
         Button(
             frame,
             text="+/-",
-            command=partial(self._toggle_tag_is_visible, tags_frame, entry),
+            command=partial(self._toggle_tag_is_visible, entry),
             bg=self.bg_color,
             fg=self.fg_color,
             activebackground=self.bg_color,
             activeforeground=self.fg_color,
         ).grid(row=0, column=1, padx=5, pady=5)
 
-    def _add_tags_selection_clear_button(self, tags_frame: LabelFrame) -> None:
+    def _add_tags_selection_clear_button(self) -> None:
         Button(
-            tags_frame,
+            self.tags_frame,
             text="Clear Selections",
             command=self._clear_tag_selections,
             bg=self.bg_color,
@@ -120,7 +123,7 @@ class GUI:
             activeforeground=self.fg_color,
         ).grid()
 
-    def _toggle_tag_is_visible(self, tags_frame: LabelFrame, entry: Entry) -> None:
+    def _toggle_tag_is_visible(self, entry: Entry) -> None:
         """Toggle the visibility of the tag itself on the UI."""
         target = entry.get()
 
@@ -141,10 +144,10 @@ class GUI:
         if not is_target_in_tags:
             db.create_tag(target)
 
-        self._update_tags_in_tags_frame(tags_frame)
+        self._update_tags_in_tags_frame()
 
-    def _update_tags_in_tags_frame(self, frame: LabelFrame) -> None:
-        self._clear_frame(frame)
+    def _update_tags_in_tags_frame(self) -> None:
+        self._clear_frame(self.tags_frame)
         self._init_tags_frame()
 
     def _clear_frame(self, frame: LabelFrame) -> None:
@@ -152,31 +155,57 @@ class GUI:
         if hasattr(frame, "pack_forget"):
             frame.pack_forget()
 
-    def _populate_tags_in_tags_frame(self, tags_frame: LabelFrame) -> None:
-        tags = db.fetch_tags()
-        for tag in tags:
-            if not tag.is_hidden:
-                Checkbutton(
-                    tags_frame,
-                    text=tag.name,
-                    variable=tag.is_selected,
-                    command=partial(db.toggle_tag_selection, tag),
-                    selectcolor=self.bg_color,
-                    bg=self.bg_color,
-                    fg=self.fg_color,
-                    activebackground=self.bg_color,
-                    activeforeground=self.fg_color,
-                ).grid()
+    def _populate_tags_in_tags_frame(self) -> None:
+        for tag in self._get_visible_tags():
+            Checkbutton(
+                self.tags_frame,
+                text=tag.name,
+                variable=tag.is_selected,
+                command=partial(self.handle_tag_selection, tag),
+                selectcolor=self.bg_color,
+                bg=self.bg_color,
+                fg=self.fg_color,
+                activebackground=self.bg_color,
+                activeforeground=self.fg_color,
+            ).grid()
+
+    def handle_tag_selection(self, tag: db.Tag) -> None:
+        db.toggle_tag_selection(tag)
+        self._clear_frame(self.files_frame)
+        self._init_files_frame()
 
     def _clear_tag_selections(self) -> None:
         db.clear_all_tag_selections()
-        # Should there be a self.clear_frame(tags_frame) here?
+
+        self._clear_frame(self.tags_frame)
         self._init_tags_frame()
+
+        self._clear_frame(self.files_frame)
+        self._init_files_frame()
+
+    def _get_visible_tags(self) -> List[db.Tag]:
+        visible_tags = []
+
+        for tag in db.fetch_tags():
+            if not tag.is_hidden:
+                visible_tags.append(tag)
+
+        return visible_tags
+
+    def _get_selected_tag_names(self) -> List[str]:
+        selected_tags = []
+
+        for tag in db.fetch_tags():
+            if tag.is_selected.get():
+                selected_tags.append(tag.name)
+
+        return selected_tags
 
     # File results stuff
 
-    def _add_files_frame(self) -> None:
+    def _init_files_frame(self) -> None:
         row, column = self.FILES_FRAME_POS
+
         frame = LabelFrame(self.gui, text="Files", bg=self.bg_color, fg=self.fg_color)
         frame.grid(row=row, column=column, padx=5, pady=5)
 
@@ -184,17 +213,21 @@ class GUI:
             side=TOP, anchor=N, padx=5, pady=5, ipadx=1, ipady=1
         )
 
-        files = db.fetch_files()
-        for file in files:
-            Button(
-                frame,
-                text=file.name,
-                command=partial(os.startfile, file.path),
-                bg=self.bg_color,
-                fg=self.fg_color,
-                activebackground=self.bg_color,
-                activeforeground=self.fg_color,
-            ).pack()
+        selected_tags = set(self._get_selected_tag_names())
+
+        for file in db.fetch_files():
+            if selected_tags.issubset(file.tags):
+                Button(
+                    frame,
+                    text=file.name,
+                    command=partial(os.startfile, file.path),
+                    bg=self.bg_color,
+                    fg=self.fg_color,
+                    activebackground=self.bg_color,
+                    activeforeground=self.fg_color,
+                ).pack()
+
+        self.files_frame = frame
 
     # Debugging stuff
 
