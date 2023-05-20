1bdc7832d5925f1c6591c8209ebd45897786f1c7
holychowders <theholychowders@gmail.com>
2023-05-20 13:27:15 -0500

    fix: comment out broken icon-setting line
    Not sure why this doesn't work. Just need things to build, not looking
    into it right now.

--- file_manager/gui2/__init__.py
+++ file_manager/gui2/__init__.py
@@ -7,7 +7,7 @@ from gui2.tags_frame import add_tags_frame
 def run() -> None:
     gui = Tk()
     gui.title("File Manager")
-    gui.iconbitmap("assets/main-icon-512px-colored.ico")
+    # gui.iconbitmap("assets/main-icon-512px-colored.ico")
     gui.rowconfigure(0, weight=1)
     gui.columnconfigure(1, weight=1)
 
